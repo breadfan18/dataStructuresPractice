b21a3d6cbc7bcb77f9a9acc515bf7be26409a20e
Swaroop Uprety <breadfan18@gmail.com>
2020-07-11 17:10:40 -0400

    added exercize files

--- java/src/NewClass.java
+++ java/src/NewClass.java
@@ -2,20 +2,26 @@ public class NewClass {
 
 
     public static void main(String[] args) {
-        int[] bubbleSort = {12, 8, 7, 5, 2};
+//        int[] bubbleSort = {12, 8, 7, 5, 2};
+//
+//        int[] sorted = {};
+//
+//        for (int i = 0; i < bubbleSort.length-1; i++) {
+//            for (int j = 0; j < bubbleSort.length - 1 - i; j++) {
+//                if (bubbleSort[j] > bubbleSort[j+1]) {
+//                    int temp = bubbleSort[j+1];
+//                    bubbleSort[j + 1] = bubbleSort[j];
+//                    bubbleSort[j] = temp;
+//                    System.out.println(temp);
+//                }
+//            }
+//        }
 
-        int[] sorted = {};
+        int[] selectionSort = {9, 2, 8, 12, 7};
+        for (int i = 0; i < selectionSort.length; i++) {
 
-        for (int i = 0; i < bubbleSort.length-1; i++) {
-            for (int j = 0; j < bubbleSort.length - 1 - i; j++) {
-                if (bubbleSort[j] > bubbleSort[j+1]) {
-                    int temp = bubbleSort[j+1];
-                    bubbleSort[j + 1] = bubbleSort[j];
-                    bubbleSort[j] = temp;
-                    System.out.println(temp);
-                }
-            }
         }
+
     }
 
 
