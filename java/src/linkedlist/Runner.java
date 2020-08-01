88bfac1c45583f22c8906908e5faa52397b37dc8
Swaroop Uprety <breadfan18@gmail.com>
2020-07-31 20:35:15 -0400

    trying to resolve the google  method

--- java/src/linkedlist/Runner.java
+++ java/src/linkedlist/Runner.java
@@ -4,6 +4,7 @@ import com.sun.media.sound.RIFFInvalidDataException;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 import sun.plugin2.ipc.windows.WindowsIPCFactory;
 
+import javax.print.attribute.standard.Finishings;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -19,23 +20,21 @@ public class Runner {
 //        System.out.println(newString);
 
         String test = "del";
-        System.out.println(in1020(test));
+        System.out.println(google());
 
 
 
 
     }
 
-    public static String in1020(String  str) {
+    public static Character google() {
+        //Goal is to find the first repeating character!
+        String input = "google";
+        int distance;
+        char finalChar = 0;
 
-        if (str.length() >=4){
-            String sub = str.substring(1, 4);
-            if (sub.equalsIgnoreCase("del")) {
-                return str.replace("del", "");
-            }
-        }
 
-        return str;
+        return null;
     }
 }
 
