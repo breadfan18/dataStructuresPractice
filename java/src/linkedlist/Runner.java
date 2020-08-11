cc49b7ca756a45564a88d29dc855d35cf7ed42e0
Swaroop Uprety <breadfan18@gmail.com>
2020-08-11 09:02:16 -0400

    cleared code from the practice method

--- java/src/linkedlist/Runner.java
+++ java/src/linkedlist/Runner.java
@@ -29,53 +29,10 @@ public class Runner {
     }
 
     public static void practice() {
-        String testString1 = "Lorem Ipsum!! is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
-        String testString = "Lorem Ipsum!! is simply, dummy, text lorem the but, text! are of! was.";
-
-        String myRegex = "[^a-zA-Z ]";
-
-        String trimmed = testString1.trim().toLowerCase().replaceAll(myRegex, "");
-
-        String[] words = trimmed.split(" ");
-
-        HashMap<String, Integer> myHash = new HashMap<>();
-
-        for (int i = 0; i < words.length; i++) {
-            String currentWord = words[i];
-            if (!isCommon(currentWord) && myHash.containsKey(currentWord)) {
-                myHash.put(currentWord, myHash.get(currentWord) + 1);
-            }else if (!isCommon(currentWord)){myHash.put(currentWord, 1);}
-        }
-
-        for (String word : myHash.keySet()) {
-            System.out.println("Word: " + word + " | Count: " + myHash.get(word));
-        }
-
-        int max  = 0;
-        String maxWord = "";
-        for (String wordd : myHash.keySet()) {
-            if (myHash.get(wordd) > max) {
-                max = myHash.get(wordd);
-                maxWord = wordd;
-            }
-        }
-
-        System.out.println("Maxword: "  + maxWord + " | Repeated " + max + " times");
 
     }
 
-    public static boolean isCommon(String word) {
-        String[] commonWords = {"the", "a", "or", "an", "it", "and", "but", "is", "are", "of", "on", "to", "was", "were", "in", "that", "i", "your", "his", "their", "her", "you", "me", "all"};
-
-        for (String commonWord : commonWords) {
-            if (word.equalsIgnoreCase(commonWord)){
-                return true;
-            }
-
-        }
-        return false;
-    }
 }
 
 
