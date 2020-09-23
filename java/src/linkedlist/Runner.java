0b00f65c4c78ed7fd1daa62e5bea7a165d433c4a
Swaroop Uprety <breadfan18@gmail.com>
2020-09-23 14:23:52 -0400

    midday commit

--- java/src/linkedlist/Runner.java
+++ java/src/linkedlist/Runner.java
@@ -12,20 +12,26 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        int[] test = {4, 5}; // {5 ,7, 2}
+        int[] test = {4,5,6}; // {5 ,7, 2}
         int[] test1 = {1, 5, 7, 7, 6};
 
-        System.out.println(practice(test));
+        System.out.println(Arrays.toString(practice(test)));
     }
 
 
 
-    public static boolean practice(int[] nums) {
-      //so if either the first element or thee second element = 2 or 3, then return true
-        if (nums[0] != 2 && nums[1] != 2 && nums[0] != 3 && nums[1] != 3) {
-            return true;
-        }
-        return false;
+    public static int[] practice(int[] nums) {
+      //get length of the array
+        //int number of elements to add = length -1.
+
+        int numsLength = nums.length;
+        int lengthOfNewArray = numsLength*2;
+        int lastElementInOriginalArray = nums[numsLength - 1];
+        int[] newNums = new int[lengthOfNewArray];
+
+        newNums[lengthOfNewArray-1] = lastElementInOriginalArray;
+
+        return newNums;
     }
 
 
